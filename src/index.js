41df8a462be2dc52fa690ebe890c78d96a7f3379
Day Dreamer <kselvakumar001@gmail.com>
2021-03-15 12:39:04 +0000

    babel config setup done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-const test = Array(7).fill(null);
-console.log(test);
+const Animal = function ({ name, energy }) {
+    this.name = name;
+    this.energy = energy;
+};
+
+Animal.prototype.eat = function (energy) {
+    this.energy += energy;
+};
+
+const catObject = { name: 'Cat', energy: 7 };
+
+console.log(catObject);
+
+const cat = new Animal(catObject);
+
+console.log(cat);
+
+cat.eat(5);
+
+console.log(cat);
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'development',
+    devtool: false,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader' },
+            },
+        ],
+    },
+    devServer: {
+        contentBase: 'dist/',
+    },
+};
